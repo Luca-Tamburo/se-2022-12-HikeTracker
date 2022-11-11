b5f0319ff4fa204dd55fccb351a7f7608760e2bf
Luca Tamburo <luca.jonk7@hotmail.it>
2022-11-11 21:14:26 +0100

    Added tests
    - Added tests of: Home page, Filter, App Container and Navbar

--- client/src/components/ui-core/Navbar/Navbar.test.jsx
+++ client/src/components/ui-core/Navbar/Navbar.test.jsx
@@ -12,8 +12,40 @@
 
 // Imports
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { Router, MemoryRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
 import Navbar from './Navbar';
+
+//Mock react-bootstrap
+jest.mock('react-bootstrap', () => {
+    const Navbar = (props) => {
+        return (
+            <nav>{props.children}</nav>
+        );
+    }
+
+    Navbar.Brand = (props) => {
+        return (
+            <div>{props.children}</div>
+        )
+    }
+
+    const Container = (props) => {
+        return (
+            <div>{props.children}</div>
+        )
+    }
+
+    const Button = ({ children, ...props }) => {
+        return (
+            <button {...props}>{children}</button>
+        )
+    }
+
+    return ({ Navbar, Button, Container });
+})
 
 const links = [
     { title: '', url: '/' },
@@ -23,17 +55,23 @@ const links = [
 
 describe('NavbarComponent', () => {
 
-    test('Check if Navbar have logo', () => {
+    it('Check if Navbar have logo', () => {
         render(<Navbar />, { wrapper: MemoryRouter });
         expect(screen.getByAltText(/Logo Icon/)).toBeInTheDocument();
 
     });
 
-    test.each(links)(
-        'Check if Navbar have %s link', (link) => {
-            render(<Navbar />, { wrapper: MemoryRouter });
+    it.each(links)(
+        'Check if Navbar have %s link and if it works', (link) => {
+            const history = createMemoryHistory();
+            render(
+                <Router location={history.location} navigator={history}>
+                    <Navbar />
+                </Router>);
             const linkDom = screen.getByRole('link', { name: link.title });
-            expect(linkDom).toHaveAttribute('href', link.url)
+            expect(linkDom).toHaveAttribute('href', link.url);
+            userEvent.click(screen.getByRole('link', { name: link.title }));
+            expect(history.location.pathname).toBe(link.url)
         }
     );
 });
--- client/src/components/ui-core/RegisterForm/RegisterForm.jsx
+++ client/src/components/ui-core/RegisterForm/RegisterForm.jsx
@@ -9,16 +9,17 @@
 * All rights reserved.
 * --------------------------------------------------------------------
 */
-//*
+
 // Imports
 import { useState, useContext } from 'react';
 import { Button, Spinner, Row, Col } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
-import { Field, Formik, Form} from 'formik';
+import { Field, Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
 // Services
 import api from '../../../services/api'
+
 // Components
 import Input from "../../utils/Input"
 
@@ -33,9 +34,7 @@ const RegisterForm = (props) => {
     const [, , setDirty] = useContext(AuthContext);
     const notify = useNotification(); // Notification handler
     const navigate = useNavigate(); // Navigation handler
-    
 
-    
     const handleSubmit = (credentials) => {
         setLoading(true);
         api.addNewUser(credentials)
@@ -52,54 +51,64 @@ const RegisterForm = (props) => {
         username: Yup.string().required('Username requested'),
         email: Yup.string().email('Email is not valid').required('Email needed'),
         password: Yup.string().required('Password needed'),
-        name:  Yup.string().when(props.currRole, (currRole)=> { return currRole !== 'Hiker' ?
-                                ".required('Name needed')":''}),
-        number: Yup.string().when(props.currRole, (currRole)=> { return currRole !== 'Hiker' ?
-                               ".required('Phone number needed')":''}),
-        surname: Yup.string().when(props.currRole, (currRole)=> { return currRole !== 'Hiker' ?
-                                 '.required("Name needed")':''})
+        name: Yup.string().when(props.currRole, (currRole) => {
+            return currRole !== 'Hiker' ?
+                ".required('Name needed')" : ''
+        }),
+        number: Yup.string().when(props.currRole, (currRole) => {
+            return currRole !== 'Hiker' ?
+                ".required('Phone number needed')" : ''
+        }),
+        surname: Yup.string().when(props.currRole, (currRole) => {
+            return currRole !== 'Hiker' ?
+                '.required("Name needed")' : ''
+        })
     });
 
     return (
-        <Formik validateOnMount initialValues={{ username: '', email:'', password: '', role:'', name:'', surname:''  }} validationSchema={RegisterSchema} onSubmit={(values) => handleSubmit(values)}>
+        <Formik validateOnMount initialValues={{ username: '', email: '', password: '', role: '', name: '', surname: '' }} validationSchema={RegisterSchema} onSubmit={(values) => handleSubmit(values)}>
             {({ touched, isValid }) => {
                 const disableSubmit = (!touched.username && !touched.password && !touched.email) || !isValid || loading;
                 return (
                     <Form>
                         <Row>
                             <Col>
-                        <Input className="mt-3" id="signup-username" name="username" type="text" placeholder="Insert your username" label="Username" />
-                        </Col>
-                        <Col>
-                    
-                        <Field className="mt-3" name="role" as="select"  placeholder="Insert your role" label="Role">
-                           
-                            <option value="Hiker" onClick={props.setCurrRole('Hiker')}>Hiker</option>
-                            <option value="Local guide" onClick={props.setCurrRole('Local guide')}>Local guide</option>
-                            <option value="Hut worker" onClick={props.setCurrRole('Hut worker')}>Hut worker</option>
-                        </Field></Col>
-                        </Row>
-                        <Row><Col> 
-                        <Input className="mt-3" id="signup-email" name="email" type="email" placeholder="Insert your email" label="Email" />
-                        </Col><Col>
-                        <Input className="mt-3" id="signup-password" name="password" type="password" placeholder="Insert your password" label="Password" />
-                        </Col>
-                        </Row>
-                        {props.currRole !== 'Hiker' ? <><Row>
-                        <Col> 
-                        <Input className="mt-3" id="signup-name" name="name" type="text" placeholder="Insert your name" label="Name" />
-                        </Col><Col>
-                        <Input className="mt-3" id="signup-surname" name="surname" type="text" placeholder="Insert your surname" label="Surname" />
-                        </Col>
+                                <Input className="mt-3" id="signup-username" name="username" type="text" placeholder="Insert your username" label="Username" />
+                            </Col>
+                            <Col>
+                                <Field className="mt-3" name="role" as="select" placeholder="Insert your role" label="Role">
+                                    <option value="Hiker" onClick={props.setCurrRole('Hiker')}>Hiker</option>
+                                    <option value="Local guide" onClick={props.setCurrRole('Local guide')}>Local guide</option>
+                                    <option value="Hut worker" onClick={props.setCurrRole('Hut worker')}>Hut worker</option>
+                                </Field>
+                            </Col>
                         </Row>
                         <Row>
-                        <Input className="mt-3" id="signup-number" name="number" type="text" placeholder="Insert your phone number" label="Phone number" />
-                        </Row></>:''}
+                            <Col>
+                                <Input className="mt-3" id="signup-email" name="email" type="email" placeholder="Insert your email" label="Email" />
+                            </Col>
+                            <Col>
+                                <Input className="mt-3" id="signup-password" name="password" type="password" placeholder="Insert your password" label="Password" />
+                            </Col>
+                        </Row>
+                        {props.currRole !== 'Hiker' ? <>
+                            <Row>
+                                <Col>
+                                    <Input className="mt-3" id="signup-name" name="name" type="text" placeholder="Insert your name" label="Name" />
+                                </Col>
+                                <Col>
+                                    <Input className="mt-3" id="signup-surname" name="surname" type="text" placeholder="Insert your surname" label="Surname" />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Input className="mt-3" id="signup-number" name="number" type="text" placeholder="Insert your phone number" label="Phone number" />
+                            </Row>
+                        </> : ''}
                         <Row>
-                        <Button variant="primary" type="submit" className='p-3 rounded-3 mt-4 w-100 fw-semibold' disabled={disableSubmit}>
-                            {loading && <Spinner animation='grow' size='sm' as='span' role='status' aria-hidden='true' className='me-2' />}
-                            Sign up
-                        </Button>
+                            <Button variant="primary" type="submit" className='p-3 rounded-3 mt-4 w-100 fw-semibold' disabled={disableSubmit}>
+                                {loading && <Spinner animation='grow' size='sm' as='span' role='status' aria-hidden='true' className='me-2' />}
+                                Sign up
+                            </Button>
                         </Row>
                     </Form>
                 );
@@ -108,5 +117,5 @@ const RegisterForm = (props) => {
     );
 }
 
-export default RegisterForm; 
+export default RegisterForm;
 //*/
