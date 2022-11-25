7c9bc246133c13c9fbc58c7a8e9a76bf43100674
GABRIELE SAMBIN S303399 <s303399@studenti.polito.it>
2022-11-25 16:44:29 +0100

    added support for mockDB while e2e testing, mock db now starts with 2 users

---
+++ server/indexTest.js
@@ -0,0 +1,23 @@
+/*
+* -------------------------------------------------------------------- 
+*
+* Package:         server
+* File:            indexTest.js
+*
+* Copyright (c) 2022 - se2022-Team12
+* All rights reserved.
+* --------------------------------------------------------------------
+*/
+
+
+// This file exists for the sole purpose of end to end testing, if you somehow end up here while not testing you did something wrong
+
+'use strict';
+
+
+const { setTesting }=require('./test/mockDB/iAmTesting');
+setTesting(1);
+
+
+const app=require('./utils/appUtil');
+
