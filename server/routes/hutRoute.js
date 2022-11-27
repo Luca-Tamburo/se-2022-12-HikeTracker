c47af6a7aa96ef365d07e21a78325c8cbdaf957a
antoniocolelli <colelli16@gmail.com>
2022-11-27 10:45:27 +0100

    fix: hut route

--- server/routes/hutRoute.js
+++ server/routes/hutRoute.js
@@ -22,32 +22,6 @@ const sessionUtil = require("../utils/sessionUtil");
 const isLoggedInLocalGuide = sessionUtil.isLoggedInLocalGuide;
 
 /**
- * Put parking point into the system
- */
-
-router.post('/parking',
-    isLoggedInLocalGuide,
-    check("title").exists().withMessage("This field is mandatory").bail().isString(),
-    check("description").exists().withMessage("This field is mandatory").bail().isString(),
-    check("latitude").exists().withMessage("This field is mandatory").bail().isNumeric(),
-    check("longitude").exists().withMessage("This field is mandatory").bail().isNumeric(),
-    check("altitude").exists().withMessage("This field is mandatory").bail().isFloat({ gt: 0 }),
-    check("city").exists().withMessage("This field is mandatory").bail().isString(),
-    check("province").exists().withMessage("This field is mandatory").bail().isString(),
-    check("region").exists().withMessage("This field is mandatory").bail().isString(),
-    checksValidation, async (req, res) => {
-        try {
-            //create parking point in db
-            await pointDao.addPoint(req.body.title, req.body.description, "parking lot", req.body.latitude, req.body.longitude, req.body.altitude, req.body.city, req.body.province, req.body.region);
-            return res.status(201).json({ message: "Parking inserted in the system" });
-        } catch (error) {
-            res.status(503).json({ error: `Service unavailable` });
-        }
-    });
-
-
-
-/**
 * Put hut into the system
 */
 
@@ -55,9 +29,12 @@ router.post('/hut',
     isLoggedInLocalGuide,
     check("title").exists().withMessage("This field is mandatory").bail().isString(),
     check("photoFile").exists().withMessage("This field is mandatory").bail().isString(),
-    check("roomsNumber").exists().withMessage("This field is mandatory").bail().isNumeric(),
-    check("bedsNumber").exists().withMessage("This field is mandatory").bail().isNumeric(),
-    check("phoneNumber").exists().withMessage("This field is mandatory").bail().isString(),
+    check("roomsNumber").exists().withMessage("This field is mandatory").bail().isInt({ min: 0 }),
+    check("bedsNumber").exists().withMessage("This field is mandatory").bail().isInt({ min: 0 }),
+    check("phoneNumber").exists().withMessage("This field is mandatory").bail()
+        .isString().withMessage("This field must be a string (consider the prefix of the phone number)").bail()
+        .trim().isLength({ min: 2 }).withMessage("This field is a string and must be from 2 characters").bail()
+        .trim().matches(/^[+0-9][0-9 ]+$/).withMessage("This field must contain only numbers, the + for prefixes and spaces"),
     check("latitude").exists().withMessage("This field is mandatory").bail().isNumeric(),
     check("longitude").exists().withMessage("This field is mandatory").bail().isNumeric(),
     check("altitude").exists().withMessage("This field is mandatory").bail().isFloat({ gt: 0 }),
