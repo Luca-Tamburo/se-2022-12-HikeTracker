df6a96183e60fa5c95490551779bc1f81aa0f921
FabioTrovero <s301587@studenti.polito.it>
2022-12-09 17:55:33 +0100

    add Map add api to update the start/end point

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -87,6 +87,15 @@ const api = {
         })
     },
 
+    putLinkStartEndPoint: (hikeId,formData) => {
+        console.log(formData)
+        return new Promise((resolve, reject) => {
+            axios.put(SERVER_URL + `hikeStartEnd/${hikeId}`,formData, { withCredentials: true })
+                .then((res) => resolve(res.data))
+                .catch((err) => reject(err));
+        })
+    },
+
     addHike: (formData) => {
         return new Promise((resolve, reject) => {
             axios.post(SERVER_URL + 'hikes', formData, { withCredentials: true })
--- client/src/views/LocalGuidePage/LinkPoint/LinkStartEndPoint.jsx
+++ client/src/views/LocalGuidePage/LinkPoint/LinkStartEndPoint.jsx
@@ -12,11 +12,12 @@
 
 // Imports
 import { useState, useEffect } from 'react'
-import { Row, Col, Spinner } from 'react-bootstrap'
+import { Row, Col, Spinner,Button} from 'react-bootstrap'
 import { useParams } from "react-router-dom";
 
 // Components - uiCore
 import InfoPoint from '../../../components/ui-core/InfoPoint/InfoPoint';
+import MapStartEndLink from '../../../components/ui-core/locate/MapStartEndLink';
 
 // Services
 import api from '../../../services/api';
@@ -29,6 +30,10 @@ const LinkStartEndPoint = () => {
     const [loading, setLoading] = useState(true);
     const [points, setPoints] = useState([]);
     const notify = useNotification();
+    const [currentStart, setCurrentStart] = useState();
+    const [currentEnd, setCurrentEnd] = useState();
+    const [start, setStart] = useState();
+    const [end, setEnd] = useState();
     const { hikeId } = useParams();
     // USA QUESTE PER MODIFICARE I VALORI DEL CURRENT POINT + CAMBIA ANCHE LE PROPS CHE PASSI AGLI INFOPOINT
     // const [currentStartPoint, setCurrentStartPoint] = useState(points.currentStartPoint);
@@ -38,6 +43,10 @@ const LinkStartEndPoint = () => {
         api.getLinkStartEndPoint(hikeId)
             .then(points => {
                 setPoints(points);
+                setCurrentEnd(points.currentEndPoint)
+                setCurrentStart(points.currentStartPoint)
+                setEnd(points.currentEndPoint)
+                setStart(points.currentStartPoint)
             })
             .catch(err => {
                 setPoints([]);
@@ -45,6 +54,34 @@ const LinkStartEndPoint = () => {
             })
             .finally(() => setLoading(false));
     }, []); //eslint-disable-line react-hooks/exhaustive-deps
+
+    const handelReset= ()=>{
+        setCurrentEnd(end)
+        setCurrentStart(start)
+    }
+
+    const handleSave = ()=>{
+        let data;
+        if(currentStart.id !== start.id && currentEnd.id !== end.id){
+            data = {
+                startPointId: currentStart.id,
+                endPointId:currentEnd.id
+            }
+        }
+        else if( currentStart.id === start.id)
+        {
+            data = {
+                endPointId:currentEnd.id
+            }
+
+        }
+        else{
+            data = {
+                endPointId:currentEnd.id
+            }
+        }
+        api.putLinkStartEndPoint(hikeId,data)
+    }
 
     if (!loading) {
         return (
@@ -56,17 +93,24 @@ const LinkStartEndPoint = () => {
                     <Col xs={10} sm={8} md={6} lg={4} className='mb-3 mb-sm-0'>
                         <div>
                             <h4 className='m-3 fst-italic'>Start Point</h4>
-                            <InfoPoint points={points.currentStartPoint} eventKeyNumber={'0'} />
+                            <InfoPoint points={currentStart} eventKeyNumber={'0'} />
                         </div>
                         <div className='mt-4'>
                             <h4 className='m-3 fst-italic'>End Point</h4>
-                            <InfoPoint points={points.currentEndPoint} eventKeyNumber={'1'} />
+                            <InfoPoint points={currentEnd} eventKeyNumber={'1'} />
                         </div>
                     </Col>
                     <Col xs={{ span: 10, offset: 1 }} lg={{ span: 4, offset: 3 }}>
-                        <h1 className="fw-bold">La mappa sta arrivando</h1>
+                        <MapStartEndLink points={points} setEnd={setCurrentEnd} setStart={setCurrentStart} currentEnd={currentEnd} currentStart={currentStart} />
+                        <Row>
+                    <div className="d-flex justify-content-evenly mt-2">
+                        <Button onClick={() => {handelReset()}}>Reset</Button>
+                        <Button onClick={() => {handleSave()}}>Save</Button>
+                    </div>
+                </Row>
                     </Col>
                 </Row>
+                
             </div>
         );
     } else {
