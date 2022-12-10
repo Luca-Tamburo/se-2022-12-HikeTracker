de3ad1125f96f3d8afd5d7d6f327e605f3437ae9
Luca Tamburo <luca.jonk7@hotmail.it>
2022-12-10 22:17:00 +0100

    SonarCloud coverage report

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -4,8 +4,10 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     theme: 'dark',
+    experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config)
+      return config
     },
   },
 });
