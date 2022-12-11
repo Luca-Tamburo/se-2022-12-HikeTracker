aa0a623d3a4d2fff67229a8e13a6c261b8411d05
antoniocolelli <63364810+antoniocolelli@users.noreply.github.com>
2022-12-11 10:51:44 +0100

    fix: hikePointDao.js for merge

--- server/dao/hikePointDao.js
+++ server/dao/hikePointDao.js
@@ -53,3 +53,32 @@ exports.deleteHikePointCorrispondance = (hikeId, pointId) => {
         });
     });
 }
+exports.getRefPointsByHikeId = (hikeId) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT * FROM HikePoint,Point WHERE Point.id=HikePoint.pointId and HikePoint.hikeId = ?';
+        db.all(sql, [hikeId], (err, rows) => {
+            if (err) {
+                reject(err);
+            }
+            else if (rows === undefined)
+                resolve(undefined);
+            else {
+                const hikes = rows.map((p) => (
+                    {
+                        id: p.id,
+                        name: p.name,
+                        type: p.type,
+                        description: p.description,
+                        latitude: p.latitude,
+                        longitude: p.longitude,
+                        altitude: p.altitude,
+                        city: p.city,
+                        province: p.province,
+                        region: p.region
+                    }
+                ));
+                resolve(hikes);
+            }
+        });
+    });
+}
